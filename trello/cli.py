fbecaed361235105299d4dea9dbf043d2940f333
Jeremy Lesser <jeremylessermore@gmail.com>
2021-01-08 00:27:09 -0700

    Successfully read keys.txt and connected to trello api

--- trello/cli.py
+++ trello/cli.py
@@ -3,12 +3,42 @@ import requests
 import json
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
 
-def cli(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo('Hello %s!' % name)
+def cli():
+    keys_file = open("keys.txt")
+    lines = keys_file.readlines()
+    key = lines[0].rstrip()
+    token = lines[1].rstrip()
+    board = lines[2].rstrip()
+
+    print("Key is " , key)
+    print("token is " , token)
+    print("board is " , token)
+
+    boardurl = "https://api.trello.com/1/boards/" + board + "/lists"
+    labelurl = "https://api.trello.com/1/boards/" + board + "/labels"
+    cardUrl = "https://api.trello.com/1/cards"
+
+    query = {
+        'key': key,
+        'token': token
+    }
+
+    boardResponse = requests.request(
+        "GET",
+        boardurl,
+        params=query
+    )
+
+    labelResponse = requests.request(
+        "GET",
+        labelurl,
+        params=query
+    )
+
+    print(boardResponse)
+
+    print(labelResponse)
+
+
 
