63d47e7b67259ce6e37c3ff3a02ee6c6f31ca9fb
Jeremy Lesser <jeremylessermore@gmail.com>
2021-01-08 00:05:05 -0700

    Working example of CLI

---
+++ trello/cli.py
@@ -0,0 +1,14 @@
+import click
+import requests
+import json
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+
+def cli(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo('Hello %s!' % name)
+
